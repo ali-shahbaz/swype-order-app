3d7c9d9f1bb0e1568adebb5eb634d7b916a93b1f
Habib ur Rehman <habibgaba@live.com>
2021-12-31 00:13:14 +0500

    Fixed First Page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Header from '../components/head';
 import Link from 'next/link';
 import { apiSettings } from '../configs/api-settings';
 import Image from 'next/image';
+import { useState } from 'react';
+import { LocationOutline } from "react-ionicons";
+
 
 Home.defaultProps = {
   name: 'Home',
@@ -11,6 +14,28 @@ Home.defaultProps = {
 }
 
 export default function Home({ data }) {
+
+  const actualRestaurantDate = data.payload.restaurants;
+  const [restaurantData, setRestaurantData] = useState(actualRestaurantDate);
+  const SearchRestaurant = (search) => 
+  {
+      let filterd = actualRestaurantDate;
+      if(search)
+      {
+          search = search.toLocaleLowerCase();
+          filterd = actualRestaurantDate.filter(item=> {
+
+              if(item.companyName && item.companyName.toLocaleLowerCase().indexOf(search) > -1)
+                return true;
+                
+              if(item.companyLocation && item.companyLocation.toLocaleLowerCase().indexOf(search) > -1)
+                return true;
+              
+              return false
+            });
+      }
+      setRestaurantData(filterd);
+  };
   return <>
     <Header title="Restaurants">
       <style>
@@ -19,18 +44,39 @@ export default function Home({ data }) {
         }`}
       </style>
     </Header>
+    <div class="section confirmed-email mt-2">
+            <form action="#">
+                <div class="card card-border">
+                    <div class="card-body">
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label class="label">Search</label>
+                                <input type="email" class="form-control" required="" onChange={(e)=>SearchRestaurant(e.target.value)}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+     </div>
     <div className="section">
-      <ul className="row">
+      <ul className="row ulRestaurant">
         {
-          data.payload.restaurants.map(item => {
-            return <div key={item.companyId} className="col-4 mt-2">
+          restaurantData.map(item => {
+            return <div key={item.companyId} className="col-sm-4 mt-2">
               <Link href={`/restaurant/${item.companyId}`}>
                 <a>
-                  <div className="card item-card card-border p-0">
+                  <div className="card item-card card-border p-0 restaurant-card">
                     <div>
                       <Image src={item.companyLogoUrl} height={300} width={400} layout="responsive" priority className="card-img-top" alt="image" />
                     </div>
-                    <h4>{item.companyName}</h4>
+                    <h4  className="card-title">{item.companyName}</h4>
+                    { 
+                      item.companyLocation && 
+                      <h6 className="card-text">
+                        <LocationOutline className="switchSVGColor" width="20px" height="20px" />
+                          {item.companyLocation}
+                      </h6>
+                    }
                   </div>
                 </a>
               </Link>
